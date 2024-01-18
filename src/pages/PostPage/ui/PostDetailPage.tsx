fa8b9830cee17b3ad52f76daef9e613ebfb7c8cc
Dmitry <arhidiman@mail.ru>
2024-01-19 02:43:50 +0700

    feat: loader

--- src/pages/PostPage/ui/PostDetailPage.tsx
+++ src/pages/PostPage/ui/PostDetailPage.tsx
@@ -5,6 +5,7 @@ import {setPost} from "@/pages/PostPage/store";
 import {useGetSinglePostQuery} from "@/shared/api/queries/postsApi.ts";
 import {Button} from "@/shared/ui/Button/Button.tsx";
 import {Post} from "@/entities/Post";
+import {Loader} from "@/shared/ui/Loader/Loader.tsx";
 import type {RootState} from "@/app/store";
 import './PostDetailPage.scss'
 
@@ -13,26 +14,15 @@ export const PostDetailPage: React.FC = () => {
     const dispatch = useDispatch()
     const {id} = useParams<{ id: string }>()
     const postId = id ? +id : undefined
-    const {data} = useGetSinglePostQuery(postId)
+    const {data,isLoading} = useGetSinglePostQuery(postId)
     const {postPage} = useSelector((state: RootState) => state)
 
-    // const {post} = useSelector()
-
-    useEffect(() => {
-        dispatch(setPost(data))
-    }, [data]);
-
-    return (
-        <div className='post-detail-page'>
-
-            <div className='post-detail-page-header'>
-                <Link to='/'>
-                    <Button>Назад</Button>
-                </Link>
-                <h2>Детали поста номер {id}</h2>
-            </div>
-            {
-                postPage.post &&
+    const post = () => {
+        if(isLoading) {
+            return <Loader/>
+        }
+        if(postPage.post) {
+            return(
                 <Post
                     id={postPage.post.id}
                     detailed
@@ -40,6 +30,24 @@ export const PostDetailPage: React.FC = () => {
                     title={postPage.post.title}
                     body={postPage.post.body}
                 />
+            )
+        }
+    }
+
+    useEffect(() => {
+        dispatch(setPost(data))
+    }, [data]);
+
+    return (
+        <div className='post-detail-page'>
+            <div className='post-detail-page-header'>
+                <Link to='/'>
+                    <Button>Назад</Button>
+                </Link>
+                <h2>Детали поста номер {id}</h2>
+            </div>
+            {
+                post()
             }
         </div>
 
